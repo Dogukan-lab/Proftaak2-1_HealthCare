aeeaeb8cec863fabb5e820990ba21c9061aa9bd0
N0cturnal <dogukan-akyazi@hotmail.com>
2020-09-16 10:50:01 +0200

    Removed unnecessary files so that the gitignore works on the development

--- HealthCar3/ConsoleApp1/Program.cs
+++ HealthCar3/ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double planckConstant = 6.62607004 * Math.Pow(10, -34);
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The constant of planck is: {0}m2 kg/s", planckConstant);
         }
     }
 }
