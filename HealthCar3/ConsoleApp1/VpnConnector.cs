025cca4b3d855e8cdbde38896f1d25d0605a83e9
MickWerf <mickwerf@outlook.com>
2020-09-18 10:55:35 +0200

    Added comments to the code

--- HealthCar3/ConsoleApp1/VpnConnector.cs
+++ HealthCar3/ConsoleApp1/VpnConnector.cs
@@ -9,14 +9,18 @@ namespace ConsoleApp1
     class VpnConnector
     {
         private TcpClient client;
+        private MessageParser parser;
         private NetworkStream stream = default;
         private static readonly string address = "145.48.6.10";
         private static readonly int port = 6666;
+        private Boolean connected;
 
         public NetworkStream Stream { get; private set; }
 
         public VpnConnector()
         {
+            parser = new MessageParser(this);
+            connected = false;
             Connect();
         }
 
@@ -26,6 +30,7 @@ namespace ConsoleApp1
             {
                 client = new TcpClient();
                 client.Connect(address, port);
+                connected = true;
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
@@ -34,19 +39,35 @@ namespace ConsoleApp1
 
         public void Send(Payload payload)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload));
-            String message = JsonConvert.SerializeObject(payload);
-            Console.WriteLine(message);
-            try
+            if (connected)
             {
-                stream = client.GetStream();
-                stream.Write(BitConverter.GetBytes(bytes.Length), 0, 4);
-                stream.Write(bytes, 0, bytes.Length);
-                Listen();
-            } catch (Exception ex)
+                byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload));
+                String message = JsonConvert.SerializeObject(payload);
+                Console.WriteLine(message);
+                try
+                {
+                    stream = client.GetStream();
+                    stream.Write(BitConverter.GetBytes(bytes.Length), 0, 4);
+                    stream.Write(bytes, 0, bytes.Length);
+                    Listen();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } else
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Connection error. Attempting to reconnect.");
+                Connect();
+                if (connected)
+                {
+                    Console.WriteLine("Connection restored. Sending message.");
+                } else
+                {
+                    Console.WriteLine("Reconnection failed, check connection settings.");
+                }
             }
+            
             
         }
 
@@ -56,6 +77,7 @@ namespace ConsoleApp1
             byte[] bytes = new byte[bytesize];
             while (stream.CanRead)
             {
+                //temp code, needs to be amplified with overflow protection, message buffering and parser calls.
                 int received = stream.Read(bytes, 0, bytes.Length);
                 Console.WriteLine(received);
                 Console.WriteLine(Encoding.Unicode.GetString(bytes));
