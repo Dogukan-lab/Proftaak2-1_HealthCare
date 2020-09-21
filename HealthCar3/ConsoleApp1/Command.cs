95486a6e36a9cdd447bedc8e56cd09d806076ae9
Nicholas <nkw.leroux@student.avans.nl>
2020-09-21 14:13:59 +0200

    Added data classes

---
+++ HealthCar3/ConsoleApp1/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Command
+    {
+
+        private String id;
+
+        private List<String> data;
+
+        public Command(String id, List<String> data)
+        {
+            this.id = id;
+            this.data = data;
+        }
+    }
+}
---
+++ HealthCar3/ConsoleApp1/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Data : IPayload
+    {
+        public string id;
+        public List<String> data;
+
+        public Data(string id, String session, String key)         {
+            this.id = id;
+            data = new List<string>();
+            data.Add(session);
+            data.Add(key);
+        }
+
+     /*   public Data(String id, String dest, Command data)
+        {
+            this.id = id;
+            this.dest = dest;
+
+        }*/
+
+    }
+}
