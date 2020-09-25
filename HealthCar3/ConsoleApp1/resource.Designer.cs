67546f90ad914e68255e934108b025c5b373ed06
N0cturnal <dogukan-akyazi@hotmail.com>
2020-09-25 21:38:31 +0200

    Resource cartoon added.

--- HealthCar3/ConsoleApp1/resource.Designer.cs
+++ HealthCar3/ConsoleApp1/resource.Designer.cs
@@ -4057,6 +4057,16 @@ namespace ConsoleApp1 {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Pony_cartoon3 {
+            get {
+                object obj = ResourceManager.GetObject("Pony_cartoon3", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap savanna_dry_d {
