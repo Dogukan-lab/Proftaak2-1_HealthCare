68557d327d122d6f054ddee746eaac973f6aa4f9
jurre <jurrevlas@icloud.com>
2020-09-30 15:41:08 +0200

    Added Doctor Login and Home
    The basics without comments

--- HealthCar3/ConsoleApp1/Program.cs
+++ HealthCar3/ConsoleApp1/Program.cs
@@ -4,99 +4,107 @@ using System;
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
-
+using DoctorGui;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Application.Run(new SimDocLogin());
+
             /*
-                        TempListenerClass listener = new TempListenerClass();
-                        SimForm simForm = null;
+            TempListenerClass listener = new TempListenerClass();
+            //SimForm simForm = null;
+            SimDoc simdoc = null;
+            // Select connector option
+            ConnectorOption connector = null;
+            string cInput = "";
+            while (cInput == string.Empty)
+            {
+                Console.WriteLine("Select bluetooth or simulator: |B|S|");
+                cInput = Console.ReadLine();
+                if (cInput.ToUpper() == "B")
+                    connector = new Bluetooth("Avans Bike AC74", "Avans Bike AC74", listener);
+                else if (cInput.ToUpper() == "S")
+                {
+                    // Do the gui setup
+                    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
 
-                        // Select connector option
-                        ConnectorOption connector = null;
-                        string cInput = "";
-                        while (cInput == string.Empty)
-                        {
-                            Console.WriteLine("Select bluetooth or simulator: |B|S|");
-                            cInput = Console.ReadLine();
-                            if (cInput.ToUpper() == "B")
-                                connector = new Bluetooth("Avans Bike AC74", "Avans Bike AC74", listener);
-                            else if (cInput.ToUpper() == "S")
-                            {
-                                // Do the gui setup
-                                Application.SetHighDpiMode(HighDpiMode.SystemAware);
-                                Application.EnableVisualStyles();
-                                Application.SetCompatibleTextRenderingDefault(false);
-                                simForm = new SimForm();
-                                connector = new Simulator(listener, simForm);
-                            }
-                            else
-                                cInput = "";
-                        }
+                    //simForm = new SimForm();
+
+                    simdoc = new SimDoc();
+                    connector = new Simulator(listener, simdoc);
+                }
+                else
+                    cInput = "";
+            }
 
 
-                        var simulator = connector as Simulator;
-                        if (simulator != null)
-                        {
-                            // Start the update thread
-                            simulator.updateThread.Start();
+            var simulator = connector as Simulator;
+            if (simulator != null)
+            {
+                // Start the update thread
+                simulator.updateThread.Start();
 
-                            // Start the command thread
-                            Thread consoleThread = new Thread(new ParameterizedThreadStart(ReadInput));
-                            consoleThread.Start(connector);
+                // Start the command thread
+                Thread consoleThread = new Thread(new ParameterizedThreadStart(ReadInput));
+                consoleThread.Start(connector);
 
-                            // Start the gui
-                            Application.Run(simForm);
-                        }
-                        else
-                        {
-                            Thread.Sleep(4000);
-                            // No need to run this on this different thread
-                            ReadInput(connector);
-                        }
-                    }
-
-                    public static void ReadInput(Object connectorOption)
-                    {
-                        var connector = connectorOption as ConnectorOption;
-
-                        string input = "";
-                        while (!input.Equals("quit"))
-                        {
-                            Console.WriteLine("Commands: \n" +
-                            "- quit (Quit application)\n" +
-                            "- res (Send resistance)");
-
-                            input = Console.ReadLine();
-                            switch (input)
-                            {
-                                case "quit": // Quit the application
-                                    return;
-                                case "res": // Send resistance to the bike
-                                    Console.WriteLine("Amount of resistance: ");
-                                    input = Console.ReadLine();
-                                    connector.WriteResistance(float.Parse(input));
-                                    Console.WriteLine("");
-                                    break;
-                                default: // Unknown command
-                                    Console.WriteLine("Not a valid command.");
-                                    break;
-                            }
-                        }
-                    }*/
-
-            VpnConnector connector = new VpnConnector(new JsonSerializerSettings());
-
-            /*Thread listenThread = new Thread(new ThreadStart(connector.Listen));
-            listenThread.Start();*/
-
-            //CommandCenter cc = new CommandCenter();
-
+                // Start the gui
+                Application.Run(simdoc);
+            }
+            else
+            {
+                Thread.Sleep(4000);
+                // No need to run this on this different thread
+                ReadInput(connector);
+            }
+            */
             Console.Read();
+            
         }
-    }
+
+        public static void ReadInput(Object connectorOption)
+        {
+            var connector = connectorOption as ConnectorOption;
+
+            string input = "";
+            while (!input.Equals("quit"))
+            {
+                Console.WriteLine("Commands: \n" +
+                "- quit (Quit application)\n" +
+                "- res (Send resistance)");
+
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "quit": // Quit the application
+                        return;
+                    case "res": // Send resistance to the bike
+                        Console.WriteLine("Amount of resistance: ");
+                        input = Console.ReadLine();
+                        connector.WriteResistance(float.Parse(input));
+                        Console.WriteLine("");
+                        break;
+                    default: // Unknown command
+                        Console.WriteLine("Not a valid command.");
+                        break;
+                }
+            }
+        }
+
+        VpnConnector connector = new VpnConnector(new JsonSerializerSettings());
+
+        /*Thread listenThread = new Thread(new ThreadStart(connector.Listen));
+        listenThread.Start();*/
+
+        //CommandCenter cc = new CommandCenter();
+
+
+
+        }
 }
 
---
+++ HealthCar3/DoctorGui/SimDocHome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DoctorGui
+{
+    public partial class SimDocHome : Form
+    {
+        public SimDocHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
