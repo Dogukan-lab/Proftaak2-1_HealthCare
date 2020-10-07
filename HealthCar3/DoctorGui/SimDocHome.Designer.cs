decdf6a7a2c1947e7650008041c9fbfbaefb5233
jurre <jurrevlas@icloud.com>
2020-10-07 14:56:18 +0200

    BikeHome and BikeLogin Basics
    the basics are done

--- HealthCar3/DoctorGui/SimDocHome.Designer.cs
+++ HealthCar3/DoctorGui/SimDocHome.Designer.cs
@@ -28,42 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimDocHome));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Home = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.Docter = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.DoctorWelcome = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(2, 5);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.Home);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1378, 99);
+            this.panel1.Size = new System.Drawing.Size(433, 1021);
             this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 621);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button3.Size = new System.Drawing.Size(433, 210);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Patient History";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(0, 405);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(459, 210);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Bike     Patient";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.Home.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Home.FlatAppearance.BorderSize = 0;
+            this.Home.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Home.Image = ((System.Drawing.Image)(resources.GetObject("Home.Image")));
+            this.Home.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Home.Location = new System.Drawing.Point(0, 189);
+            this.Home.Name = "Home";
+            this.Home.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Home.Size = new System.Drawing.Size(433, 210);
+            this.Home.TabIndex = 1;
+            this.Home.Text = "Home";
+            this.Home.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Home.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.Docter);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(433, 183);
+            this.panel3.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Georgia", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(1230, 7);
+            this.button1.Location = new System.Drawing.Point(240, 196);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 92);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "X";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Docter
+            // 
+            this.Docter.AutoSize = true;
+            this.Docter.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Docter.Location = new System.Drawing.Point(101, 76);
+            this.Docter.Name = "Docter";
+            this.Docter.Size = new System.Drawing.Size(170, 65);
+            this.Docter.TabIndex = 0;
+            this.Docter.Text = "Docter";
+            this.Docter.Click += new System.EventHandler(this.Docter_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.DoctorWelcome);
+            this.panel4.Location = new System.Drawing.Point(470, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1135, 141);
+            this.panel4.TabIndex = 2;
+            // 
+            // DoctorWelcome
+            // 
+            this.DoctorWelcome.AutoSize = true;
+            this.DoctorWelcome.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DoctorWelcome.Location = new System.Drawing.Point(146, 34);
+            this.DoctorWelcome.Name = "DoctorWelcome";
+            this.DoctorWelcome.Size = new System.Drawing.Size(464, 78);
+            this.DoctorWelcome.TabIndex = 0;
+            this.DoctorWelcome.Text = "welkom doctor...";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.FlatAppearance.BorderSize = 0;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.Font = new System.Drawing.Font("Georgia", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ExitButton.Location = new System.Drawing.Point(1611, 12);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(194, 129);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // SimDocHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1381, 835);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1817, 1033);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SimDocHome";
-            this.Text = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.SimDocHome_Load);
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +193,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label Docter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Home;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label DoctorWelcome;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
--- HealthCar3/DoctorGui/SimDocHome.cs
+++ HealthCar3/DoctorGui/SimDocHome.cs
@@ -10,9 +10,26 @@ namespace DoctorGui
 {
     public partial class SimDocHome : Form
     {
-        public SimDocHome()
+        public SimDocHome(string username)
         {
             InitializeComponent();
+            DoctorWelcome.Text = "Welcome Docter " + username;
+        }
+
+        private void Docter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SimDocHome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Environment.Exit(0);
         }
     }
 }
