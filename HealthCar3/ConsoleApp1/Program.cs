3fa7eb9c9ba3df1c43f4f59ece8a370ec0324b31
Dogukan <dogukan-akyazi@hotmail.com>
2020-10-13 21:29:11 +0200

    Nothing of importance

--- HealthCar3/ConsoleApp1/Program.cs
+++ HealthCar3/ConsoleApp1/Program.cs
@@ -15,74 +15,76 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            TempListenerClass listener = new TempListenerClass();
-            ServerConnection serverCon = new ServerConnection();
-            SimForm simForm = null;
-            //Application.Run(new SimDocLogin());
+            // TempListenerClass listener = new TempListenerClass();
+            // ServerConnection serverCon = new ServerConnection();
+            // SimForm simForm = null;
+            // //Application.Run(new SimDocLogin());
+            //
+            // string cInput = "";
+            // Console.WriteLine("Select: \n" +
+            //     "- login\n" +
+            //     "- register\n");
+            // cInput = Console.ReadLine();
+            // if (cInput == "login")
+            // {
+            //     Console.WriteLine("Name: ");
+            //     cInput = Console.ReadLine();
+            //     Console.WriteLine("Id: ");
+            //     string idInput = Console.ReadLine();
+            //     serverCon.LoginToServer(cInput, idInput);
+            // }
+            // else if (cInput == "register")
+            // {
+            //     Console.WriteLine("Name: ");
+            //     cInput = Console.ReadLine();
+            //     serverCon.RegisterToServer(cInput);
+            // }
+            //
+            // cInput = "";
+            // // Select connector option
+            // ConnectorOption connector = null;
+            // while (cInput == string.Empty)
+            // {
+            //     Console.WriteLine("Select bluetooth or simulator: |B|S|");
+            //     cInput = Console.ReadLine();
+            //     if (cInput.ToUpper() == "B")
+            //         connector = new Bluetooth("Avans Bike AC74", "Avans Bike AC74", listener, serverCon);
+            //     else if (cInput.ToUpper() == "S")
+            //     {
+            //         // Do the gui setup
+            //         Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //         Application.EnableVisualStyles();
+            //         Application.SetCompatibleTextRenderingDefault(false);
+            //         simForm = new SimForm();
+            //         connector = new Simulator(listener, serverCon, simForm);
+            //     }
+            //     else
+            //         cInput = "";
+            // }
+            //
+            // serverCon.SetConnectorOption(connector);
+            //
+            // var simulator = connector as Simulator;
+            // if (simulator != null)
+            // {
+            //     // Start the update thread
+            //     simulator.updateThread.Start();
+            //
+            //     // Start the command thread
+            //     Thread consoleThread = new Thread(new ParameterizedThreadStart(ReadInput));
+            //     consoleThread.Start(serverCon);
+            //
+            //     // Start the gui
+            //     Application.Run(simForm);
+            // }
+            // else
+            // {
+            //     Thread.Sleep(4000);
+            //     // No need to run this on this different thread
+            //     ReadInput(serverCon);
+            // }
+            VpnConnector connector = new VpnConnector(new JsonSerializerSettings());
 
-            string cInput = "";
-            Console.WriteLine("Select: \n" +
-                "- login\n" +
-                "- register\n");
-            cInput = Console.ReadLine();
-            if (cInput == "login")
-            {
-                Console.WriteLine("Name: ");
-                cInput = Console.ReadLine();
-                Console.WriteLine("Id: ");
-                string idInput = Console.ReadLine();
-                serverCon.LoginToServer(cInput, idInput);
-            }
-            else if (cInput == "register")
-            {
-                Console.WriteLine("Name: ");
-                cInput = Console.ReadLine();
-                serverCon.RegisterToServer(cInput);
-            }
-
-            cInput = "";
-            // Select connector option
-            ConnectorOption connector = null;
-            while (cInput == string.Empty)
-            {
-                Console.WriteLine("Select bluetooth or simulator: |B|S|");
-                cInput = Console.ReadLine();
-                if (cInput.ToUpper() == "B")
-                    connector = new Bluetooth("Avans Bike AC74", "Avans Bike AC74", listener, serverCon);
-                else if (cInput.ToUpper() == "S")
-                {
-                    // Do the gui setup
-                    Application.SetHighDpiMode(HighDpiMode.SystemAware);
-                    Application.EnableVisualStyles();
-                    Application.SetCompatibleTextRenderingDefault(false);
-                    simForm = new SimForm();
-                    connector = new Simulator(listener, serverCon, simForm);
-                }
-                else
-                    cInput = "";
-            }
-
-            serverCon.SetConnectorOption(connector);
-
-            var simulator = connector as Simulator;
-            if (simulator != null)
-            {
-                // Start the update thread
-                simulator.updateThread.Start();
-
-                // Start the command thread
-                Thread consoleThread = new Thread(new ParameterizedThreadStart(ReadInput));
-                consoleThread.Start(serverCon);
-
-                // Start the gui
-                Application.Run(simForm);
-            }
-            else
-            {
-                Thread.Sleep(4000);
-                // No need to run this on this different thread
-                ReadInput(serverCon);
-            }
         }
 
         public static void ReadInput(Object serverConnection)
@@ -146,7 +148,6 @@ namespace ConsoleApp1
                 }
             }
         }
-        /*VpnConnector connector = new VpnConnector(new JsonSerializerSettings());*/
     }
 }
 
