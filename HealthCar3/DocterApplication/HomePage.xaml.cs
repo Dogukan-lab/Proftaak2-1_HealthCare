dfc9b281101e2ed481ea0729c4b2c9bbf927e5e1
jurre <jurrevlas@icloud.com>
2020-10-16 17:55:06 +0200

    Created WPF DocterApplication
    Created WPF with login and home

---
+++ HealthCar3/DocterApplication/HomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DocterApplication
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        String Username { get; set; }
+        public HomePage(String Username)
+        {
+            InitializeComponent();
+            this.Username = Username;
+            WelcomeDocter.Text = "Welcome Doctor " + this.Username;
+        }
+
+
+        //Dragging Window
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        //Close Application
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
